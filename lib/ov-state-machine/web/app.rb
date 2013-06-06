dbcd705cd185036babf5c493ce42d62e2e3f20f7
Thomas Brus <thomas.brus@me.com>
2013-06-06 20:17:22 +0200

    Got rid of some duplication

--- lib/ov-state-machine/web/app.rb
+++ lib/ov-state-machine/web/app.rb
@@ -4,30 +4,21 @@ require 'sass'
 
 module OVStateMachine
   module Web
-    module Engines
-      class CoffeeEngine < Sinatra::Base
-        set :views, File.dirname(__FILE__) + '/assets/javascripts'
+    class App < Sinatra::Base
+      class AssetEngine < Sinatra::Base
+        set :views, File.dirname(__FILE__) + '/assets'
         
         get "/javascripts/*.js" do
-          coffee params[:splat].first.to_sym
+          coffee "javascripts/#{params[:splat].first}".to_sym
         end
-      end
-    end
 
-    module Engines
-      class SassEngine < Sinatra::Base
-        set :views, File.dirname(__FILE__) + '/assets/stylesheets'
-        
         get '/stylesheets/*.css' do
-          sass params[:splat].first.to_sym
+          sass "stylesheets/#{params[:splat].first}".to_sym
         end
       end
-    end
 
-    class App < Sinatra::Base
       # use Faye::RackAdapter, mount: '/faye', timeout: 25
-      use Engines::SassEngine
-      use Engines::CoffeeEngine
+      use AssetEngine
 
       get '/' do
         erb :index
